63cf27b0e251abcad8f6c35769c6f0d8f24b2a22
Dhruv-Mehndhiratta <dhruv.mehndhiratta@meesho.com>
2023-07-14 13:01:56 +0530

    initial comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import ChatBot from "react-simple-chatbot";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: 'flex', justifyContent: 'center', marginTop: 70}}>
+      <ChatBot
+       
+        steps={[
+          {
+            id: "1",
+            message: "What number I am thinking?",
+            trigger: "2",
+          },
+          {
+            id: "2",
+            options: [
+              { value: 1, label: "Number 1", trigger: "4" },
+              { value: 2, label: "Number 2", trigger: "3" },
+              { value: 3, label: "Number 3", trigger: "3" },
+            ],
+          },
+          {
+            id: "3",
+            message: "Wrong answer, try again.",
+            trigger: "2",
+          },
+          {
+            id: "4",
+            message: "Awesome! You are a telepath!",
+            end: true,
+          },
+        ]}
+      />
     </div>
   );
 }
